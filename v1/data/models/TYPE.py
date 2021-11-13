543b20e2bed642b4817b851f976b0a511bb88494
Austen Myers <austenc.id@pm.me>
2021-11-12 19:06:13 -0700

    remove v1 files from v2

--- v1/data/models/TYPE.py
+++
@@ -1,6 +0,0 @@
-class Typing:
-    def __init__(typing, data):
-        typing.name = data['name']
-        typing.strengths = data['strengths']
-        typing.weaknesses = data['weaknesses']
-        typing.immunity = data['immunities']
