401c8ad731618f9427a22582b1e66d17e657c680
Austen Myers-Flachson <austenc.id@pm.me>
2022-02-15 20:13:09 -0700

    added fields to invoke model and updated styling

--- Capstone/Portal/RELAY/models.py
+++ Capstone/Portal/RELAY/models.py
@@ -1,16 +1,40 @@
 from django.db.models import *
+from django.utils.text import format_lazy
 from colorful.fields import RGBColorField as ColorField
 from django.contrib.auth import get_user_model
-
+from utils import (
+    generators as gen,
+    extractors as extract,
+    retrievers as ret,
+)
 # Create your models here.
+'''
+> - request ID = CharField(max_length=8)
+> - site admin data
+>   - website url = URLField(max_length=20)
+>   - site description = CharField(max_length=20)
+> - site type = CharField(max_length=20, choices=[(eCommerce, 'eCommerce'), (blog, 'blog'), (other, 'other')])
+> - site logo = ImageField(upload_to='path')
+> - site icons = CharField(choices=[(tbd, '')])
+> - additional site images = {'label': '', 'image': ''}
+> - build status = IntegerField()
+'''
 
 
 class Invocation(Model):
     user = ForeignKey(get_user_model(), on_delete=CASCADE,
                       related_name='invocations')
     title = CharField(max_length=20)
+    purpose = CharField(default='', max_length=25, choices=[('Blog', 'share my opinions'), (
+        'Org', 'promote my organization'), ('Shop', 'sell my goods and/or services')])
     background_color = ColorField(default='#FFFFFF')
+    card_background_color = ColorField(default='#FFFFFF')
+    border_color = ColorField(default='#FFFFFF')
     font_color = ColorField(default='#FFFFFF')
+    link_color = ColorField(default='#FFFFFF')
+    primary_font = CharField(
+        choices=ret.get_google_fonts(), max_length=99, default='',
+        help_text='visit https://fonts.google.com/ to browse')
 
     def __str__(self):
         return f'{self.user} - {self.title}'
--- Capstone/Portal/utils/retrievers.py
+++ Capstone/Portal/utils/retrievers.py
@@ -8,10 +8,16 @@ def get_patron(regcode):
     return patron
 
 
-def get_patron_API(regcode):
-    import requests
-    base_url = 'http://127.0.0.1:8000/api/'
-    url = f'{base_url}patrons/{regcode}'
-    response = requests.get(url)
-    data = response
-    print(data)
+def get_google_fonts():
+    import requests as api
+    from utils.api_keys import GoogleFonts
+    url = f'https://www.googleapis.com/webfonts/v1/webfonts?key={GoogleFonts}'
+    response = api.get(url)
+    count = 0
+    json = response.json()  # .keys() = ['kind', 'items']
+    items = json['items']
+    fonts = []
+    for item in items:
+        font = (item['family'], item['family'])
+        fonts.append(font)
+    return fonts
