735dada226e21b9a3c69189e46d1c3c595d3f686
Austen Myers-Flachson <austenc.id@pm.me>
2022-02-16 21:24:48 -0700

    theme overall

--- Capstone/Portal/_assets/_style/theme.js
+++ Capstone/Portal/_assets/_style/theme.js
@@ -1,24 +1,45 @@
+defaultTheme()
+
 const useColor = document.getElementById("use-color").innerText;
 if (useColor === "True") {
-  cascadeColors();
+  const color = document.getElementById("fav-color").innerText;
+  const links = document.getElementsByTagName("a");
+  setColors(links, color);
 }
 
-function cascadeColors(){
-    const color = document.getElementById("fav-color").innerText;
-    const links = document.getElementsByTagName("a");
-    setColors(links, color);
-    const color_ocean = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--ocean");
-    const color_void = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--void");
-    document.documentElement.style.setProperty("--wave", color_ocean);
-    document.documentElement.style.setProperty("--ocean", color_void);
-    document.documentElement.style.setProperty("--void", color);
-  }
-  function setColors(elements, color) {
+function defaultTheme(){
+  document.body.classList.add('bg-void')
+  document.body.classList.add('font-acme')
+  document.getElementById('block-index').classList.add('bg-ocean')
+  document.getElementById('block-index').classList.add('text-stone')
+  document.getElementById('main').classList.add('bg-wave')
+  document.getElementById('header').classList.add('border-b2-void')
+  document.getElementById('footer').classList.add('border-t2-void')
+  const LINKS = document.getElementsByTagName('a')
+  const FORMS = document.getElementsByTagName('form')
+  const SPANS = document.getElementsByTagName('span')
+  const TITLE = document.getElementById('site-title')
+  setColorClasses(LINKS, "text-sand")
+  setColorClasses(FORMS, "border-2-void")
+  setColorClasses(SPANS, "text-rock")
+  TITLE.classList.add('text-stone')
+}
+
+function setColors(elements, color) {
+    console.log(elements)
     for (let i = 0; i < elements.length; i++) {
+      let elemID = elements.item(i).id
+      if (elemID != 'site-title'){
       elements.item(i).style.color = color;
+      }
+    }
+  }
+function setColorClasses(elements, className) {
+    // console.log(elements)
+    for (let i = 0; i < elements.length; i++) {
+      let elemID = elements.item(i).id
+      if (elemID != 'site-title'){
+        elements.item(i).classList.add(className)
+    }
     }
   }
