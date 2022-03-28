69c9c7924503a75b775ddffc4ac30b16f6a2da22
austenc-id <austenc.id@pm.me>
2022-03-28 00:34:25 -0600

    org for repo merge

--- working/django-02/users_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'users'
-urlpatterns = [
-    path('user/login', views.login, name='login'),
-    path('user/register', views.register, name='register'),
-]
